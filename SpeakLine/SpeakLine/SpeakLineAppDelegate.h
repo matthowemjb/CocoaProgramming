6e47519d8e2cce4419fea57dab83b5cd6e6fd954
Matt Howe <mhowemegis@gmail.com>
2012-05-29 17:25:33 -0400

    Speakline Compiles and runs

--- SpeakLine/SpeakLine/SpeakLineAppDelegate.h
+++ SpeakLine/SpeakLine/SpeakLineAppDelegate.h
@@ -9,7 +9,14 @@
 #import <Cocoa/Cocoa.h>
 
 @interface SpeakLineAppDelegate : NSObject <NSApplicationDelegate>
+{
+    NSSpeechSynthesizer *_speechSynth;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *textField;
+
+- (IBAction)stopIt:(id)sender;
+- (IBAction)speakIt:(id)sender;
 
 @end
--- SpeakLine/SpeakLine/SpeakLineAppDelegate.m
+++ SpeakLine/SpeakLine/SpeakLineAppDelegate.m
@@ -11,10 +11,36 @@
 @implementation SpeakLineAppDelegate
 
 @synthesize window = _window;
+@synthesize textField = _textField;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
 }
 
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        NSLog(@"init");
+        _speechSynth = [[NSSpeechSynthesizer alloc] initWithVoice:nil];
+    }
+    return self;
+}
+
+- (IBAction)speakIt:(id)sender 
+{
+    NSString *string = [_textField stringValue];
+    if ([string length] == 0) {
+        NSLog(@"The string value from %@ is 0 length", _textField);
+        return;
+    }
+    [_speechSynth startSpeakingString:string];
+    NSLog(@"Started saying: %@", string);
+}
+
+- (IBAction)stopIt:(id)sender{
+    NSLog(@"Stopping");
+    [_speechSynth stopSpeaking];
+}
 @end
