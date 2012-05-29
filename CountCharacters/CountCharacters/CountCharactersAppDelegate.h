f86ca64cbf7681abfbcf95ab1c3165bbfcd7ee4f
Matt Howe <mhowemegis@gmail.com>
2012-05-29 17:36:48 -0400

    New Project: CountCharacters

---
+++ CountCharacters/CountCharacters/CountCharactersAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CountCharactersAppDelegate.h
+//  CountCharacters
+//
+//  Created by Matt Howe on 5/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CountCharactersAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ CountCharacters/CountCharacters/CountCharactersAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  CountCharactersAppDelegate.m
+//  CountCharacters
+//
+//  Created by Matt Howe on 5/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CountCharactersAppDelegate.h"
+
+@implementation CountCharactersAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
